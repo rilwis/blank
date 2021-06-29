faac8ba8a124b7334b297dd3d1877ab2ac3c1c99
Anh Tran <anhtnt@elightup.com>
2021-06-29 10:03:00 +0700

    Add support for title tag

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <!doctype html>
 <html <?php language_attributes() ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ) ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta charset="<?php bloginfo( 'charset' ) ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head() ?>
-	<link rel="stylesheet" href="https://rilwis.github.io/bamboo/dist/light.min.css">
 </head>
 
 <body <?php body_class() ?>>
